0b8c1ca4a1df8873d418c317f844806ac3c7dfb1
takoyaki-blue <145022380+takoyaki-blue@users.noreply.github.com>
2023-12-20 09:15:40 +0900

    kadai_007をアップロード

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編_課題</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        // 配列にキーと値を代入
+        $vegetables = ['name'=>'onion', 'price'=>200, 'weight'=>160];
+
+        // 配列を出力
+        print_r($vegetables);
+        ?>
+    </p>
+</body>
